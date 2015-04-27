45731409ac8505c94a5563cfeb363318e4bdff83
Brian Christner <brian.christner@gmail.com>
2015-04-27 22:54:48 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello Cloud Community!</title>
+	<title>Hello Swisscom!</title>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
