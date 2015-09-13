4540f2d9b2b1ed9e8a0e0a410f6834852539d650
oveits <oveits@users.noreply.github.com>
2015-09-13 20:38:33 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm /run/apache2/apache2.pid # in case the server has crashed and we want to restart the container, the pid file must be removed
 chown www-data:www-data /app -R
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
